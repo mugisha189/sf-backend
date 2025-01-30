8440315d5158bfa5bc904c5f0e29a1dbcde34b2b
CK-EmbPro <kennydebrice2@gmail.com>
2025-01-30 16:13:16 +0200

    addded company products dto

--- src/company-products/dto/create-company-product.dto.ts
+++ src/company-products/dto/create-company-product.dto.ts
@@ -1,1 +1,3 @@
-export class CreateCompanyProductDto {}
+export class CreateCompanyProductDto {
+    
+}
--- src/partner-company/dto/create-partner-company.dto.ts
+++ src/partner-company/dto/create-partner-company.dto.ts
@@ -1,1 +1,29 @@
-export class CreatePartnerCompanyDto {}
+import { IsEnum, isEnum, IsNotEmpty, IsString } from "class-validator"
+import { COMPANY_TYPE } from "src/constants/constants"
+
+export class CreatePartnerCompanyDto {
+    @IsString()
+    @IsNotEmpty()
+    companyName: string
+
+    @IsEnum(COMPANY_TYPE)
+    @IsNotEmpty() 
+    companyType: keyof typeof COMPANY_TYPE
+
+    @IsString()
+    @IsNotEmpty()
+    adminFirstName: string
+
+    @IsString()
+    @IsNotEmpty() 
+    adminLastName: string
+
+    @IsString()
+    @IsNotEmpty() 
+    adminPhoneNumber: string
+
+    @IsString()
+    @IsNotEmpty() 
+    adminEmail: string
+
+}
