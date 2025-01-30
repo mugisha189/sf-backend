53506d43b748ac1d06de098eee9ef4312293ef36
CK-EmbPro <kennydebrice2@gmail.com>
2025-01-30 10:32:35 +0200

    added savingProduct module to appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { EmailModule } from './email/email.module';
+import { SavingProductModule } from './saving-product/saving-product.module';
+import { PartnerCompanyModule } from './partner-company/partner-company.module';
+import { CompanyProductsModule } from './company-products/company-products.module';
 
 @Module({
   imports: [
@@ -15,6 +18,9 @@ import { EmailModule } from './email/email.module';
     UsersModule,
     DatabaseModule,
     EmailModule,
+    SavingProductModule,
+    PartnerCompanyModule,
+    CompanyProductsModule,
   ],
 })
 export class AppModule {}
