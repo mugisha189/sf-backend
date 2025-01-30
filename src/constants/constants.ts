ee6a37f0ae6d3f03981fe5afec0f7f8005a1fabd
CK-EmbPro <kennydebrice2@gmail.com>
2025-01-30 16:11:48 +0200

    add COMPANY_TYPE enum

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -11,3 +11,9 @@ export const CHARGE_TYPE = {
     PERCENTAGE: "PERCENTAGE",
     FIXED: "FIXED",
 } as const
+
+export const COMPANY_TYPE = {
+    TELECOM: "TELECOM",
+    PETROL_STATIONS: "PETROL_STATIONS",
+    SUPER_MARKET: "SUPER_MARKET"
+}
